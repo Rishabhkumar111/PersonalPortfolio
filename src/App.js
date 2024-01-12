8ca9b8ae5db04ccabb722d86b3cd7aa705335557
Rishabh Kumar <rishabhkumar@Rishabhs-MacBook-Pro.local>
2024-01-13 00:27:21 +0530

    Skill page completed

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { NavBar } from "./components/NavBar";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skill";
 
 const App = () => {
   return (
     <>
       <NavBar />
       <Banner />
+      <Skills />
     </>
   );
 };
